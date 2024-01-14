e6627a558e0c1d655c5b2292dd99d93ff8e1e91b
elina-1201 <elina-1201@yandex.com>
2024-01-14 04:03:38 +0100

    Finished about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,57 +1,100 @@
 <script context="module">
 	// @ts-nocheck
+	import DropList from "./components/DropList.svelte";
 </script>
 
 <body>
-	<div class="w-full min-h-[90vh] flex justify-center items-center flex-col">
+	<div class="flex min-h-[90vh] w-full flex-col items-center justify-center">
+		<div
+			class="clipped mx-8 mb-6 mt-4 w-[95%] bg-primary-500 bg-fixed
+		text-secondary-300 dark:bg-secondary-300 dark:text-primary-900 sm:mt-20 lg:w-[70%]"
+		>
+			<p class="mx-6 mb-4 mt-10 px-6 py-28 text-justify text-lg sm:px-20">
+				This minimalist project is a heartfelt attempt to showcase Bosnia and Herzegovina's rich
+				natural beauty, encouraging people to explore its captivating landscapes. The goal is to
+				inspire individuals to escape the demands of urban life and immerse themselves in the
+				tranquility of our majestic mountains.
+			</p>
+		</div>
 
-		<div class="xl:w-[35%] my-12
-		lg:w-[50%] sm:w-[70%] w-[90%] 
-		border-4 border-primary-500 dark:border-secondary-300 rounded-2xl
-		flex justify-center items-center flex-col">
-
-			<p class="flex pb-1 ml-[-30%] mb-11 mt-16 font-semibold text-4xl border-b-2 border-primary-900 dark:border-secondary-100"> 
+		<div
+			class="my-6 flex w-[95%]
+		flex-col items-center justify-center
+		rounded-2xl border-2 border-primary-500 dark:border-secondary-300 sm:my-24
+		sm:w-[70%] sm:border-4 lg:w-[50%] xl:w-[35%]"
+		>
+			<p
+				class="mb-11 ml-[-30%] mt-16 flex border-b-2 border-primary-900 pb-1 text-4xl font-semibold dark:border-secondary-200"
+			>
 				Contact
 			</p>
-	
-			<form class="w-[90%]" name="contact" method="POST" netlify-honeypot="bot-field" netlify>
+
+			<form
+				class="w-[97%] sm:w-[90%]"
+				name="contact"
+				method="POST"
+				netlify-honeypot="bot-field"
+				netlify
+			>
 				<input type="hidden" name="form-name" value="contact" />
-				
+
 				<div class="field">
 					<label for="name">Name</label>
-					<input class="w-full dark:text-primary-900 p-2" type="text" name="name">
+					<input class="w-full p-2 dark:text-primary-900" type="text" name="name" />
 				</div>
-	
+
 				<div class="field">
 					<label for="email">E-mail</label>
-					<input class="w-full dark:text-primary-900 p-2" type="email" name="email">
+					<input class="w-full p-2 dark:text-primary-900" type="email" name="email" />
 				</div>
-		
+
 				<div class="field">
 					<label for="message">Message</label>
-					<textarea class="w-[100%] dark:text-primary-900 p-2" cols="30" rows="10" name="message"></textarea>
+					<textarea class="w-[100%] p-2 dark:text-primary-900" cols="30" rows="10" name="message"
+					></textarea>
 				</div>
-	
-				<div class="flex items-center justify-center ">
-					<button id="button" type="submit" class="p-2 mt-2 mb-8 w-52  cursor-pointer bg-success-400 dark:hover:bg-success-500 hover:bg-success-300  ease-in-out duration-100 rounded-md"> SEND </button>
+
+				<div class="flex items-center justify-center">
+					<button
+						id="button"
+						type="submit"
+						class="mb-8 mt-2 w-52 cursor-pointer rounded-md bg-success-400 p-2 duration-100 ease-in-out hover:bg-success-300 dark:hover:bg-success-500"
+					>
+						SEND
+					</button>
 				</div>
 			</form>
 		</div>
+
+		<DropList/>
 	</div>
 </body>
 
 <style>
-	label{
+	label {
 		margin: 0.5em 0;
 	}
 	.field {
 		margin: 1.5em 1em;
 	}
-	textarea:focus, input:focus {
+	textarea:focus,
+	input:focus {
 		outline-style: solid;
-		outline-color: #10827F;
+		outline-color: #10827f;
 	}
-	textarea, input{
-		border: solid 1px #10827F;
-	} 
+	textarea,
+	input {
+		border: solid 1px #10827f;
+	}
+
+	.clipped {
+		mask-repeat: no-repeat;
+		-webkit-mask-repeat: no-repeat;
+		mask-size: 100% 100%;
+	}
+	.clipped {
+		mask-image: url(/images/aboutMask.png);
+		-webkit-mask-image: url(/images/aboutMask.png);
+	}
+
 </style>
---
+++ src/routes/about/components/DropList.svelte
@@ -0,0 +1,93 @@
+<script>
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+</script>
+
+<div
+	class="my-5 rounded-lg bg-primary-400 text-primary-50 dark:bg-secondary-300 dark:text-primary-900 sm:mx-10"
+>
+	<Accordion>
+		<AccordionItem>
+			<svelte:fragment slot="lead">
+				<div
+					class="labeled h-10 w-10
+						bg-primary-50 dark:bg-primary-900
+						"
+				></div>
+			</svelte:fragment>
+
+			<svelte:fragment slot="summary">
+				The content on this website has been adapted and modified, drawing inspiration from the
+				following sources for informational purposes.
+			</svelte:fragment>
+
+			<svelte:fragment slot="content">
+				<div class="sm:px-10 py-5">
+					<ul class="list-disc">
+						<li>
+							<a
+								href="https://furaj.ba/top-5-najljepsih-destinacija-za-planinarenje-u-bosni-i-hercegovini/"
+								target="_blank"
+							>
+								TOP 5 najljepših destinacija za planinarenje u Bosni i Hercegovini
+							</a>
+						</li>
+
+						<li>
+							<a
+								href="https://balkans.aljazeera.net/blogs/2022/8/19/preporuka-za-planinarski-vikend-zelengora-prostranstvo-jezera-i-zelenih-livada"
+								target="_blank"
+							>
+								Preporuka za planinarski vikend: Zelengora, prostranstvo jezera i livada
+							</a>
+						</li>
+
+						<li>
+							<a href="https://www.dinarskogorje.com/zelengora.html" target="_blank">
+								Planina Zelengora
+							</a>
+						</li>
+
+						<li>
+							<a href="https://welcometokonjic.ba/prenj-planina/" target="_blank">
+								Planina Prenj
+							</a>
+						</li>
+
+						<li>
+							<a
+								href="https://furaj.ba/hajducka-vrata-na-cvrsnici-najljepsi-vidikovac-na-planinama-u-bih/"
+								target="_blank"
+							>
+								Hajdučka vrata na Čvrsnici – najljepši vidikovac na planinama u BiH
+							</a>
+						</li>
+
+						<li>
+							<a href="https://www.instagram.com/dzenad_dzino/" target="_blank">
+								Instagram of the photograph
+							</a>
+						</li>
+					</ul>
+				</div>
+			</svelte:fragment>
+		</AccordionItem>
+	</Accordion>
+</div>
+<style>
+	.labeled {
+		mask-repeat: no-repeat;
+		-webkit-mask-repeat: no-repeat;
+		mask-size: 100% 100%;
+    }
+	.labeled {
+		mask-image: url(/images/labelMask.png);
+		-webkit-mask-image: url(/images/labelMask.png);
+	}
+	li{
+		margin: 0.5em;
+		text-decoration: underline;
+	}
+	li:hover{
+		color: #10827F;
+	}    
+</style>
